6914564d91524929a0d83379d0e4dd8819e53a9d
stollander33 <107006586+stollander33@users.noreply.github.com>
2022-09-17 21:23:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuepress-theme-hope-fepq6y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vuepress-theme-hope-fepq6y)
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,27 @@
+import { defineUserConfig } from '@vuepress/cli';
+import theme from './theme';
+
+const base = (process.env.BASE as '/' | `/${string}/`) || '/';
+
+export default defineUserConfig({
+  base,
+
+  dest: './dist',
+
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Theme Demo',
+      description: 'A demo for vuepress-theme-hope',
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: '主题演示',
+      description: 'vuepress-theme-hope 的演示',
+    },
+  },
+
+  theme,
+
+  shouldPrefetch: false,
+});
